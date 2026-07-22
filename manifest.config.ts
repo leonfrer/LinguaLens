759353d3cb867dd4507dba19f679bfbbd9f4aa34
Leon <leonfrer@gamil.com>
2026-07-22 20:09:12 +0800

    chore(release): prepare v0.5.0

--- manifest.config.ts
+++ manifest.config.ts
@@ -4,7 +4,7 @@ export default defineManifest({
   manifest_version: 3,
   name: '__MSG_extensionName__',
   description: '__MSG_extensionDescription__',
-  version: '0.4.1',
+  version: '0.5.0',
   default_locale: 'en',
   options_page: 'settings.html',
   action: {
